95823ed8fb5b1287bdf0a203a2ee84a2804865ef
Rick Poleshuck <RickPoleshuck@gmail.com>
2021-08-03 09:33:26 -0400

    First pass at implementing checkstyle.
    Signed-off-by: Rick Poleshuck <RickPoleshuck@gmail.com>

--- src/main/java/application/model/ClientModel.java
+++ src/main/java/application/model/ClientModel.java
@@ -1,56 +1,55 @@
 package application.model;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import com.cloudant.client.api.ClientBuilder;
 import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
 import com.cloudant.client.api.model.Response;
 import com.cloudant.client.api.query.QueryBuilder;
 import com.cloudant.client.api.query.QueryResult;
-
-import static com.cloudant.client.api.query.EmptyExpression.empty;
-import static com.cloudant.client.api.query.Expression.eq;
-import static com.cloudant.client.api.query.Expression.gt;
-import static com.cloudant.client.api.query.Operation.and;
-
 import io.swagger.model.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.cloudant.client.api.query.Expression.eq;
 
 public class ClientModel {
-	private Database db = null;
+    private Database db;
+    private static final Logger LOG = LoggerFactory.getLogger(ClientModel.class);
 
-	public ClientModel(String url, String apiKey, String database) {
-		System.out.println(url);
-		CloudantClient client = null;
-		try {
-			client = ClientBuilder.url(new URL(url)).iamApiKey(apiKey).build();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
+    public ClientModel(String url, String apiKey, String database) {
+        LOG.debug(url);
+        CloudantClient client = null;
+        try {
+            client = ClientBuilder.url(new URL(url)).iamApiKey(apiKey).build();
+        } catch (MalformedURLException e) {
+            LOG.error("Malformed url: {}", url, e);
+        }
 
-		System.out.println("Server Version: " + client.serverVersion());
-		db = client.database(database, false);
-	}
+        LOG.debug("Server Version: {}", client.serverVersion());
+        db = client.database(database, false);
+    }
 
-	public Response save(Client client) {
-		Response resp = db.save(client);
-		return resp;
-	}
+    public Response save(Client client) {
+        Response resp = db.save(client);
+        return resp;
+    }
 
-	public Response delete(String id) {
-		Client client = db.find(Client.class, id);
-		Response resp = db.remove(client);
-		return resp;
-	}
+    public Response delete(String id) {
+        Client client = db.find(Client.class, id);
+        Response resp = db.remove(client);
+        return resp;
+    }
 
-	public Client read(String id) {
-		Client client = db.find(Client.class, id);
-		return client;
-	}
+    public Client read(String id) {
+        Client client = db.find(Client.class, id);
+        return client;
+    }
 
-	public QueryResult<Client> getAllClientsOfAttorney(String attorneyId) {
-		QueryResult<Client> qr = db.query(new QueryBuilder(eq("attorneyId", attorneyId)).build(), Client.class);
-		return qr;
-	}
+    public QueryResult<Client> getAllClientsOfAttorney(String attorneyId) {
+        QueryResult<Client> qr = db.query(new QueryBuilder(eq("attorneyId", attorneyId)).build(), Client.class);
+        return qr;
+    }
 }
